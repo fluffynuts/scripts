53c485e807ad55e10e569dd4ffd80050ab781197
Davyd McColl <davydm@gmail.com>
2019-10-29 11:18:32 +0200

    :fire: more sanity: use gnuwin32 instead of inbuilt kak

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,14 +1,19 @@
-# copy this to $PROFILE.CurrentUserCurrentHost
-
 Import-Module posh-git
 Import-Module PSReadLine
 
-Remove-Item alias:cp
-Remove-Item alias:rm
-Remove-Item alias:ls
-Remove-Item alias:rmdir
-Remove-Item alias:mv
+Remove-Item alias:cp -force
+Remove-Item alias:rm -force
+Remove-Item alias:ls -force
+Remove-Item alias:rmdir -force
+Remove-Item alias:mv -force
+Remove-Item alias:diff -force
 
 Set-Alias find C:\apps\gnuwin32\bin\find.exe
 Set-PSReadlineOption -EditMode Emacs
 Set-PSReadlineKeyHandler -Key ctrl+d -Function ViExit
+
+Function Ls-Real {
+    C:\apps\gnuwin32\bin\ls.exe --color @Args
+}
+
+Set-Alias ls Ls-Real
