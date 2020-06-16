8044b38591e4119bf9e78a68ec1cd4932f6acbf0
Davyd McColl <davydm@gmail.com>
2020-06-16 08:46:21 +0200

    :wrench: better ps profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,19 +1,18 @@
-Import-Module posh-git
-Import-Module PSReadLine
+Remove-Item alias:rm
+Remove-Item alias:ls
+Remove-Item alias:cp
+Remove-Item alias:mv
 
-Remove-Item alias:cp -force
-Remove-Item alias:rm -force
-Remove-Item alias:ls -force
-Remove-Item alias:rmdir -force
-Remove-Item alias:mv -force
-Remove-Item alias:diff -force
-
-Set-Alias find C:\apps\gnuwin32\bin\find.exe
-Set-PSReadlineOption -EditMode Emacs
-Set-PSReadlineKeyHandler -Key ctrl+d -Function ViExit
-
-Function Ls-Real {
-    C:\apps\gnuwin32\bin\ls.exe --color @Args
+Function Ls-Color {
+    . $(which ls) --color @Args
 }
+Set-Alias ls Ls-Color
 
-Set-Alias ls Ls-Real
+Import-Module posh-git
+
+# makes ctrl-d work to exit, as well as some
+#  other stuff
+Set-PSReadLineOption -EditMode vi
+# make history work the traditional
+#  way -- vi history mode has no preview
+Set-PSReadLineKeyHandler -Chord Ctrl+r -Function ReverseSearchHistory
