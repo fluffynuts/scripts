4711fa03e290e655a153b8fdc02880df5c9c574e
Davyd McColl <davydm@gmail.com>
2020-06-17 08:15:36 +0200

    :wrench: emacs mode is closer, but needs cyclic completion

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -12,7 +12,9 @@ Import-Module posh-git
 
 # makes ctrl-d work to exit, as well as some
 #  other stuff
-Set-PSReadLineOption -EditMode vi
+Set-PSReadLineOption -EditMode Emacs
 # make history work the traditional
 #  way -- vi history mode has no preview
 Set-PSReadLineKeyHandler -Chord Ctrl+r -Function ReverseSearchHistory
+Set-PSReadLineKeyHandler -Chord Tab -Function TabCompleteNext
+Set-PSReadLineKeyHandler -Chord Shift+Tab -Function TabCompletePrevious
