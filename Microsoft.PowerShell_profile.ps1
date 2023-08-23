8fc9493ac6c3b43f885742b504385bb74c8b7374
Davyd McColl <davydm@gmail.com>
2023-08-23 16:36:40 +0200

    :sparkles: include Refresh-Env cmdlet, snarfed from StackOverflow

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -17,6 +17,19 @@ function Update-Path {
                 [System.Environment]::GetEnvironmentVariable("Path","User")
 }
 
+function Refresh-Env {
+    # snarfed from https://stackoverflow.com/a/22670892
+    foreach($level in "Machine","User") {
+        [Environment]::GetEnvironmentVariables($level).GetEnumerator() | % {
+            # For Path variables, append the new values, if they're not already in there
+            if($_.Name -match 'Path$') { 
+                $_.Value = ($((Get-Content "Env:$($_.Name)") + ";$($_.Value)") -split ';' | Select -unique) -join ';'
+            }
+            $_
+        } | Set-Content -Path { "Env:$($_.Name)" }
+    }
+}
+
 <#
 .SYNOPSIS
     like cd, except it also sets the title of your terminal for
